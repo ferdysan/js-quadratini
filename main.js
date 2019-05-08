6d0da955496443f3e8ec893d9601b2e1bd37c2cf
Ferdysan <ferdysan87@gmail.com>
2019-05-08 15:15:12 +0200

    inizio del js

---
+++ main.js
@@ -0,0 +1,5 @@
+// Parte 1
+// Creare una griglia formata da 8x8 quadratini tutti bianchi.
+// 15 di questi quadratini (scelti a caso all’inizio) se cliccati diventano rossi, gli altri diventano verdi
+// Parte 2 (Bonus)
+// Sopra alla griglia deve esserci un contatore che conta quanti rossi e quanti verdi sono stati scoperti
