5b607d48688e5c34abafad17eb7fefe5e063f5ed
Ferdysan <ferdysan87@gmail.com>
2019-05-09 08:40:28 +0200

    fine senza random

--- main.js
+++ main.js
@@ -5,6 +5,25 @@
 // Sopra alla griglia deve esserci un contatore che conta quanti rossi e quanti verdi sono stati scoperti
 
 
-$('.square').each(function(){
-   $(this).addClass('square_red');
-});
+// var rosso=0;
+//
+// var verde=0;
+
+//
+// for(var i =0; i<63; i++){
+//   // var random = Math.floor(Math.random() * 10) + 1;
+  // if(rosso < 15){
+    $('.square-red').click(function(){
+       $(this).addClass('square_red');
+    });
+//     rosso ++;
+//   }
+// }
+
+
+  $('.square').click(function(){
+     $(this).addClass('square_green');
+  });
+
+
+//DECIDO A PRESCINDERE I QUADRATI VERDI E I ROSSI NON SONO RIUSCITO AD IMPOSTARLI IN RANDOM
